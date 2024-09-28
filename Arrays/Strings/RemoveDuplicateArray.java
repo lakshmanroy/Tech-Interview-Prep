58e9ad8fb75b80a8817834bb3dae170c1e6e78e0
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-09-28 14:29:42 +0530

    commit

---
+++ Arrays/Strings/RemoveDuplicateArray.java
@@ -0,0 +1,19 @@
+// package Arrays.Strings;
+
+public class RemoveDuplicateArray {
+    public static int removeDuplicate(int nums[]){
+        int count=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=nums[i-1]){
+               nums[count]=nums[i];
+               count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int nums[]= {1,1,2};
+
+        System.out.println(removeDuplicate(nums));
+    }
+}
