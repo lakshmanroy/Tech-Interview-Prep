56e9845886dd3b29be7851698755b795e2638309
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-09-30 10:51:26 +0530

    commit

---
+++ Arrays/Strings/JumpGame.java
@@ -0,0 +1,19 @@
+public class JumpGame {
+    public static boolean  canJump(int nums[]){
+        int currentdest=nums.length-1;
+        for(int i=nums.length-1;i>=0;i--){
+          if(nums[i]+i>= currentdest){
+            currentdest=i;
+          }
+        }
+        if(currentdest==0){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int nums[]= {2,3,1,1,4};
+
+        System.out.println(canJump(nums));
+    }
+}
