c84ebaa1a2e90d6cea9e566c95cae80b391a86b5
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-10-13 20:42:22 +0530

    commit

---
+++ Arrays/Strings/HappyNumber.java
@@ -0,0 +1,19 @@
+public class HappyNumber {
+    public static boolean  isHappy(int n){
+        while(n>9){
+           int sum=0;
+           while(n>0){
+            int digit=n%10;
+            sum+=digit*digit;
+            n=n/10;
+           }
+           n=sum;
+        }
+        return n==1|| n==7;
+    }
+    public static void main(String[] args) {
+        int n=19;
+
+        System.out.println(isHappy(n));
+    }
+}
