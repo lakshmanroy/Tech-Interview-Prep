6b10d59552343a3e9dbf2926afcf95ba5d7553e7
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-10-13 12:22:20 +0530

    commit

--- Arrays/Strings/Spiral_Matrix.java
+++ Arrays/Strings/Spiral_Matrix.java
@@ -29,8 +29,7 @@ public class Spiral_Matrix {
     public static void main(String[] args) {
         int matrix[][]= {{1,2,3,4},
                          {5,6,7,8},
-                        {9,10,11,12},
-                    {13,14,15,16}};
+                        {9,10,11,12}};
 
                     SpiralMatrix(matrix);
     }
