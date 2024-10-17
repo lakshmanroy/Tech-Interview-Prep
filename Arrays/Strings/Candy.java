b017d694ad72a7ab18fa5616f4e53f0dc2e2a1ed
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-10-17 17:48:06 +0530

    commit

--- Arrays/Strings/Candy.java
+++ Arrays/Strings/Candy.java
@@ -6,17 +6,25 @@ public class Candy {
       int n=ratings.length;
       int count=0;
       int arr[]= new int[n];
+
+      // Step 1: Initialize every child with 1 candy
       Arrays.fill(arr,1);
+
+      // Step 2: Traverse left to right
       for(int i=1;i<n;i++){
        if(ratings[i]>ratings[i-1]){
         arr[i]=arr[i-1]+1;
        }
       }
+
+      // Step 3: Traverse right to left
       for(int i=n-2;i>=0;i--){
         if(ratings[i]>ratings[i+1] && arr[i]<=arr[i+1]){
             arr[i]=arr[i+1]+1;
         }
       }
+      
+       // Step 4: Calculate the total number of candies
       for(int i=0;i<arr.length;i++){
         count+=arr[i];
       }
