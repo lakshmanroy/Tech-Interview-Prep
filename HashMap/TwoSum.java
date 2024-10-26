b540bb0dca92b909951d7603f5ba44a745f4740b
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-10-26 15:26:51 +0530

    commit

--- HashMap/TwoSum.java
+++ HashMap/TwoSum.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
 public class TwoSum {
     public static int[] twoSum(int nums[],int target){
         for(int i=0;i<nums.length;i++){
@@ -13,12 +16,7 @@ public class TwoSum {
         int nums[]={2,7,11,15};
         int target=9;
 
-        int result[]=twoSum(nums, target);
-        if(result.length==-1){
-            System.out.println("Not found");
-        }
-        else{
-            System.out.println(result[0]+ " "+result[1]);
-        }
+       int result[]=twoSum(nums, target);
+       System.out.println(Arrays.toString(result));
     }
 }
