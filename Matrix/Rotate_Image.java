608f7ca959b904ac31f2dcb905c3fa7de377161b
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-02 21:19:08 +0530

    commit

---
+++ Matrix/Rotate_Image.java
@@ -0,0 +1,36 @@
+public class Rotate_Image {
+    public static void rotate(int matrix[][]){
+        int n=matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][n-1-j];
+                matrix[i][n-1-j]=temp;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        
+        rotate(matrix);
+        
+        // Print rotated matrix
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+}
