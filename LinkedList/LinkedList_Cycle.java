d550f4472d4f86af02af62050475d11cc5914ef1
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-03 21:46:19 +0530

    commit

---
+++ LinkedList/LinkedList_Cycle.java
@@ -0,0 +1,38 @@
+
+public class LinkedList_Cycle {
+    public static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public static boolean isCycle(){
+        Node slow=head;
+        Node fast=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next; 
+
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+   public static void main(String[] args) {
+       head=new Node(3);
+       head.next=new Node(2);
+       head.next.next=new Node(0);
+       head.next.next.next=new Node(-4);
+       head.next.next.next.next=head;
+       //1->2->-3->1
+       System.out.println(isCycle());
+   } 
+}
