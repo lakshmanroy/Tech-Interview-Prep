89dc4a932ffbc5a3d584be6d4ad846337436aeeb
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2025-01-10 00:14:36 +0530

    commit

---
+++ Two_Pointer/ContainerWater.java
@@ -0,0 +1,25 @@
+public class ContainerWater {
+    public static int maxArea(int height[]){
+        int left=0;
+        int right=height.length-1;
+        int maxArea=0;
+
+        while(left<right){
+            int currArea=Math.min(height[left],height[right])*(right-left);
+            maxArea=Math.max(maxArea,currArea);
+    
+            if(height[left]<height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+         }
+         return maxArea;
+    }
+    public static void main(String[] args) {
+        int height[]={1,8,6,2,5,4,8,3,7};
+
+        System.out.println(maxArea(height));
+    }
+}
